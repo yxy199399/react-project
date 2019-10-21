18ca5bbe3dd2ea91f78ee3530ca2be5ef5dbe66b
yxy <2829122019@qq.com>
2019-10-21 20:34:19 +0800

    '安装redux'

--- src/double-router/child1/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-const Child1 = () => {
-  return <div>child1</div>
-}
-export default Child1
--- src/double-router/child2/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-const Child2 = () => {
-  return <div>child2</div>
-}
-export default Child2
